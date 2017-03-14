c453eefc53c61688dd80dfb5c72368e064e47816
freshield <zxdsw199182@gmail.com>
2017-03-13 21:05:41 -0400

    done basic snap on 101

--- mechanics_101/mechanics_test.py
+++ mechanics_101/mechanics_test.py
@@ -1,2 +1,10 @@
 import tensorflow as tf
 
+from tensorflow.examples.tutorials.mnist import input_data
+data_sets = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+print data_sets.train.images.shape
+print data_sets.validation.images.shape
+print data_sets.test.images.shape
+
+images_placeholder = tf.placeholder(tf.float32, shape=)
