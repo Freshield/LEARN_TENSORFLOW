d60aa7e8a0f81742e1fea274fd50657a7c3611f6
freshield <zxdsw199182@gmail.com>
2017-03-16 13:28:53 -0400

    done sub scores of graph

--- mechanics_101/tensorboard_driver.py
+++ mechanics_101/tensorboard_driver.py
@@ -7,60 +7,61 @@ max_steps = 3000
 hidden1 = 128
 hidden2 = 32
 batch_size = 100
-log_dir = 'logs/fully_connected_net_scope'
-#######################for tensorboard logs##############################3
-if tf.gfile.Exists(log_dir):
-    tf.gfile.DeleteRecursively(log_dir)
-tf.gfile.MakeDirs(log_dir)
+reg = 0.0001
 
 #from modules give the handle
 import tensorflow.examples.tutorials.mnist.input_data as input_data
 
 data_sets = input_data.read_data_sets('MNIST')
 
-images_pl, labels_pl = m.placeholder_inputs(batch_size)
+for lr in [0.01, 0.001, 0.005, 0.002]:
 
-logits = m.inference(images_pl, hidden1, hidden2)
+    with tf.Graph().as_default():
+        images_pl, labels_pl = m.placeholder_inputs(batch_size)
 
+        logits, inf_cache = m.inference(images_pl, hidden1, hidden2)
 
+        correct_nums = m.evaluation(logits, labels_pl)
 
-correct_nums = m.evaluation(logits, labels_pl)
+        loss_h = m.loss(logits, labels_pl, inf_cache, reg)
 
-# begin train
+        sess = tf.Session()
 
-sess = tf.Session()
+        train_op = m.training(loss_h, lr)
 
-loss_h = m.loss(logits, labels_pl)
+        ######################for tensorboard#############################
+        log_dir = 'logs/fully_connected_net_scope/lr' + str(lr)
+        #######################for tensorboard logs##############################3
+        if tf.gfile.Exists(log_dir):
+            tf.gfile.DeleteRecursively(log_dir)
+        tf.gfile.MakeDirs(log_dir)
+        tf.summary.scalar('loss' + str(lr), loss_h)
+        tf.summary.scalar('loss', loss_h)
+        summary = tf.summary.merge_all()
+        summary_writer = tf.summary.FileWriter(log_dir, sess.graph)
 
-train_op = m.training(loss_h, learning_rate)
+        sess.run(tf.global_variables_initializer())
 
-######################for tensorboard#############################
-tf.summary.scalar('loss', loss_h)
-summary = tf.summary.merge_all()
-summary_writer = tf.summary.FileWriter(log_dir, sess.graph)
+        for step in xrange(max_steps):
 
-sess.run(tf.global_variables_initializer())
+            feed_dict = m.fill_feed_dict(data_sets.train, images_pl, labels_pl, batch_size)
 
-for step in xrange(max_steps):
+            _, loss = sess.run([train_op, loss_h], feed_dict=feed_dict)
 
-    feed_dict = m.fill_feed_dict(data_sets.train, images_pl, labels_pl, batch_size)
+            if step % 100 == 0 or step == max_steps - 1:
+                print ('lr %.4f, step %d: loss = %.3f' % (lr, step, loss))
 
-    _, loss = sess.run([train_op, loss_h], feed_dict=feed_dict)
+                ##########################for tensorboard########################
+                summary_str = sess.run(summary, feed_dict=feed_dict)
+                summary_writer.add_summary(summary_str, step)
+                summary_writer.flush()
 
-    if step % 100 == 0 or step == max_steps - 1:
-        print ('lr %.4f, step %d: loss = %.3f' % (learning_rate, step, loss))
+            if (step + 1) == max_steps:
+                print ('Training Eval:')
+                m.do_eval(sess, correct_nums, images_pl, labels_pl, data_sets.train, batch_size)
 
-        ##########################for tensorboard########################
-        summary_str = sess.run(summary, feed_dict=feed_dict)
-        summary_writer.add_summary(summary_str, step)
-        summary_writer.flush()
+                print ('Validation Eval:')
+                m.do_eval(sess, correct_nums, images_pl, labels_pl, data_sets.validation, batch_size)
 
-    if (step + 1) == max_steps:
-        print ('Training Eval:')
-        m.do_eval(sess, correct_nums, images_pl, labels_pl, data_sets.train, batch_size)
-
-        print ('Validation Eval:')
-        m.do_eval(sess, correct_nums, images_pl, labels_pl, data_sets.validation, batch_size)
-
-        print ('Test Eval:')
-        m.do_eval(sess, correct_nums, images_pl, labels_pl, data_sets.test, batch_size)
+                print ('Test Eval:')
+                m.do_eval(sess, correct_nums, images_pl, labels_pl, data_sets.test, batch_size)
