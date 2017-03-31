7880c9d663d92de86c04717632993019770cd401
freshield <zxdsw199182@gmail.com>
2017-03-30 20:22:57 -0400

    done today

--- contrib_quickstart/contrib_test.py
+++ contrib_quickstart/contrib_test.py
@@ -22,26 +22,35 @@ test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
 # Specify that all features have real-value data
 feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
 
-print (feature_columns)
-
 # Build 3 layer DNN with 10, 20, 10 units respectively.
 classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
                                             hidden_units=[10, 20, 10],
                                             n_classes=3,
                                             model_dir="/tmp/iris_model")
 
+def get_train_inputs():
+    x = tf.constant(training_set.data)
+    y = tf.constant(training_set.target)
+
+    return x,y
+
 # Fit model.
-classifier.fit(x=tf.constant(training_set.data),
-               y=tf.constant(training_set.target),
+classifier.fit(input_fn=get_train_inputs,
                steps=2000)
 
+
+def get_test_inputs():
+    x = tf.constant(test_set.data)
+    y = tf.constant(test_set.target)
+
+    return x, y
 # Evaluate accuracy.
-accuracy_score = classifier.evaluate(x=tf.constant(test_set.data),
-                                     y=tf.constant(test_set.target))["accuracy"]
+accuracy_score = classifier.evaluate(input_fn=get_test_inputs, steps=1)["accuracy"]
 print('Accuracy: {0:f}'.format(accuracy_score))
 
 # Classify two new flower samples.
-new_samples = np.array(
+def new_samples():
+    return np.array(
     [[6.4, 3.2, 4.5, 1.5], [5.8, 3.1, 5.0, 1.7]], dtype=float)
-y = list(classifier.predict(new_samples, as_iterable=True))
-print('Predictions: {}'.format(str(y)))
+y = list(classifier.predict(input_fn=new_samples))
+print('Predictions: {}'.format(y))
