c53529e9823ddce4631ab2eafe7039599beaff4b
freshield <zxdsw199182@gmail.com>
2017-04-05 21:28:13 -0400

    find the problem of tensorflow

--- 11_tensorboard_graph/testing.py
+++ 11_tensorboard_graph/testing.py
@@ -6,8 +6,8 @@ a = tf.constant('hello\nworld', shape=[5])
 
 b = tf.string_split(a, '\n')
 
-sess = tf.InteractiveSession()
-
+sess = tf.Session()
+"""
 c = b.eval().values
 
 time_after = time.time()
@@ -17,8 +17,9 @@ print c
 print c.shape
 print time_after - time_before
 time_before = time.time()
-with tf.device('/cpu:0'):
 
+
+with tf.device('/gpu:0'):
     file = tf.gfile.Open('facebook_file')
 
     text = file.read()
@@ -27,16 +28,24 @@ with tf.device('/cpu:0'):
 
     e = tf.string_split(d, '\n')
 
-    f = tf.string_split(e.values, '\t')
+    #f = tf.string_split(e.values, '\t')
 
-    g = e.eval().values
-
-
-
+    #g = e.shape
 #print d.eval().shape
+
+sess.run(d)
 time_after = time.time()
 #print e.eval().values
 #print e.eval().values.shape
 #print g
 #print g.shape
 print time_after - time_before
+"""
+
+import tensorflow as tf
+sess = tf.InteractiveSession()
+
+with tf.device('/gpu:0'):
+    text = tf.constant("aa a", name="LEFT")
+    result = tf.string_split([text], delimiter=" ")
+print result.eval()
