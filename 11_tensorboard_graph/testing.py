e3e4bbca2342980ba3895fcc895b23f01ef75e06
freshield <zxdsw199182@gmail.com>
2017-04-05 20:08:03 -0400

    test file read and split

--- 11_tensorboard_graph/testing.py
+++ 11_tensorboard_graph/testing.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import time
 
 time_before = time.time()
-a = tf.constant('hello\nworld', shape=[10000])
+a = tf.constant('hello\nworld', shape=[5])
 
 b = tf.string_split(a, '\n')
 
@@ -15,4 +15,28 @@ time_after = time.time()
 print a.eval()
 print c
 print c.shape
+print time_after - time_before
+time_before = time.time()
+with tf.device('/cpu:0'):
+
+    file = tf.gfile.Open('facebook_file')
+
+    text = file.read()
+
+    d = tf.constant([text])
+
+    e = tf.string_split(d, '\n')
+
+    f = tf.string_split(e.values, '\t')
+
+    g = e.eval().values
+
+
+
+#print d.eval().shape
+time_after = time.time()
+#print e.eval().values
+#print e.eval().values.shape
+#print g
+#print g.shape
 print time_after - time_before
