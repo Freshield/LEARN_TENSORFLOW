229d69219b3bfea95f545b50bf4ca5588513e9de
freshield <zxdsw199182@gmail.com>
2017-04-14 17:42:50 -0400

    done the mnist beginner

--- 12_reviews/review2_mnist_beginner/minist_beginner.py
+++ 12_reviews/review2_mnist_beginner/minist_beginner.py
@@ -5,26 +5,26 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
 
-print (mnist.train.images.shape)
 
 x = tf.placeholder(tf.float32, [None, 784])
-
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
-
 y = tf.matmul(x, W) + b
 
 y_ = tf.placeholder(tf.float32, [None, 10])
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-
+cross_entropy = tf.reduce_mean(
+      tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 sess = tf.InteractiveSession()
-
 tf.global_variables_initializer().run()
 
 print sess.run([W, b])
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 for i in xrange(2000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
@@ -32,15 +32,11 @@ for i in xrange(2000):
 
     if (i % 100 == 0):
         print i
-    #    result = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
-    #    print ("the %d accuracy is: %d" % (i, result))
+        result = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+        print ("the %d accuracy is: %f" % (i, result))
 
-
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 result = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
-print ("final accuracy is: %d" % (result))
+print ("final accuracy is: %f" % (result))
 
 print sess.run([W, b])
