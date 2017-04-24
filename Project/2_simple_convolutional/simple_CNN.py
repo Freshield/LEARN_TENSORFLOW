9532fb39c8e65c3ed82d92f20bfc6a47bb4fd89f
freshield <zxdsw199182@gmail.com>
2017-04-23 21:59:21 -0400

    done simple loop

--- Project/2_simple_convolutional/simple_CNN.py
+++ Project/2_simple_convolutional/simple_CNN.py
@@ -138,125 +138,159 @@ filename = 'ciena1000.csv'
 
 dataset = pd.read_csv(filename, header=None)
 
-print dataset.values.shape
-
 train_dataset, validation_dataset, test_dataset = split_dataset(dataset, radio=0.1)
 
 data = get_batch_data(train_dataset, 100)
 
 batch_size = 100
 lr_rate = 1e-4
-max_step = 20000
+max_step = 30000
 keep_prob_v = 0.5
+conv1_depth = 64
+conv2_depth = 128
+conv3_depth = 256
+fc1_size = 2048
 
-with tf.Graph().as_default():
-    with tf.Session() as sess:
-        #inputs
-        real_C_pl = tf.placeholder(tf.float32, [None, 3100])
-        imag_C_pl = tf.placeholder(tf.float32, [None, 3100])
-        labels_pl = tf.placeholder(tf.int32, [None])
+loop_num = 2 ** 4
 
-        #reshape
-        real_C_reshape = tf.reshape(real_C_pl, [-1, 31, 100, 1])
-        imag_C_reshape = tf.reshape(imag_C_pl, [-1, 31, 100, 1])
-        image_no_padding = tf.concat([real_C_reshape, imag_C_reshape], axis=1)
+for conv1_depth in [32, 64]:
+    for conv2_depth in [64, 128]:
+        for conv3_depth in [128, 256]:
+            for keep_prob_v in [1.0, 0.5]:
 
-        tf.summary.image('input', image_no_padding, 20)
+                print '-------------------now changed-----------------'
+                print 'conv1 is', conv1_depth
+                print 'conv2 is', conv2_depth
+                print 'conv3 is', conv3_depth
+                print 'keep_prob', keep_prob_v
+                print '------------------------------------------------'
 
-        #tensors
-        images = tf.pad(image_no_padding, [[0,0], [3,3], [0,0], [0,0]], 'CONSTANT')
-        labels_one_hot = tf.one_hot(labels_pl, 3)
+                situation_now = '\n-------------------now changed-----------------\n' \
+                                'conv1 is %d\nconv2 is %d\nconv3 is %d\nkeep_prob is %.2f\n' \
+                                '------------------------------------------------' % (
+                                    conv1_depth, conv2_depth, conv3_depth, keep_prob_v)
 
-        others_pl = tf.placeholder(tf.float32, [None, 41])
+                with tf.Graph().as_default():
+                    with tf.Session() as sess:
+                        # inputs
+                        real_C_pl = tf.placeholder(tf.float32, [None, 3100])
+                        imag_C_pl = tf.placeholder(tf.float32, [None, 3100])
+                        labels_pl = tf.placeholder(tf.int32, [None])
 
-        #build graph
+                        # reshape
+                        real_C_reshape = tf.reshape(real_C_pl, [-1, 31, 100, 1])
+                        imag_C_reshape = tf.reshape(imag_C_pl, [-1, 31, 100, 1])
+                        image_no_padding = tf.concat([real_C_reshape, imag_C_reshape], axis=1)
 
-        W_conv1 = weight_variable([3, 3, 1, 32])
-        b_conv1 = bias_variable([32])
-        h_conv1 = tf.nn.relu(conv2d(images, W_conv1, 1, 'SAME') + b_conv1)
+                        tf.summary.image('input', image_no_padding, 20)
 
-        h_pool1 = max_pool_2x2(h_conv1)
+                        # tensors
+                        images = tf.pad(image_no_padding, [[0, 0], [3, 3], [0, 0], [0, 0]], 'CONSTANT')
+                        labels_one_hot = tf.one_hot(labels_pl, 3)
 
-        W_conv2 = weight_variable([3, 3, 32, 64])
-        b_conv2 = bias_variable([64])
-        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 1, 'SAME') + b_conv2)
+                        others_pl = tf.placeholder(tf.float32, [None, 41])
 
-        h_pool2 = max_pool_2x2(h_conv2)
+                        # build graph
 
-        h_pool2_pad = tf.pad(h_pool2, [[0,0], [1,1], [1,1], [0,0]], 'CONSTANT')
+                        W_conv1 = weight_variable([3, 3, 1, conv1_depth])
+                        b_conv1 = bias_variable([conv1_depth])
+                        h_conv1 = tf.nn.relu(conv2d(images, W_conv1, 1, 'SAME') + b_conv1)
 
-        W_conv3 = weight_variable([1, 1, 64, 64])
-        b_conv3 = bias_variable([64])
-        h_conv3 = tf.nn.relu(conv2d(h_pool2_pad, W_conv3, 2, 'VALID') + b_conv3)
+                        h_pool1 = max_pool_2x2(h_conv1)
 
-        h_pool3 = max_pool_2x2(h_conv3)
+                        W_conv2 = weight_variable([3, 3, conv1_depth, conv2_depth])
+                        b_conv2 = bias_variable([conv2_depth])
+                        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 1, 'SAME') + b_conv2)
+
+                        h_pool2 = max_pool_2x2(h_conv2)
+
+                        h_pool2_pad = tf.pad(h_pool2, [[0, 0], [1, 1], [1, 1], [0, 0]], 'CONSTANT')
+
+                        W_conv3 = weight_variable([1, 1, conv2_depth, conv3_depth])
+                        b_conv3 = bias_variable([conv3_depth])
+                        h_conv3 = tf.nn.relu(conv2d(h_pool2_pad, W_conv3, 2, 'VALID') + b_conv3)
+
+                        h_pool3 = max_pool_2x2(h_conv3)
+
+                        W_fc1 = weight_variable([5 * 7 * conv3_depth, fc1_size])
+                        b_fc1 = bias_variable([fc1_size])
+
+                        h_pool3_flat = tf.reshape(h_pool3, [-1, 5 * 7 * conv3_depth])
+                        h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
+
+                        keep_prob = tf.placeholder(tf.float32)
+                        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+                        W_fc2 = weight_variable([fc1_size, 3])
+                        b_fc2 = bias_variable([3])
+
+                        y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+                        cross_entropy = tf.reduce_mean(
+                            tf.nn.softmax_cross_entropy_with_logits(labels=labels_one_hot, logits=y_conv),
+                            name='xentropy')
+
+                        train_step = tf.train.AdamOptimizer(lr_rate).minimize(cross_entropy)
+                        correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(labels_one_hot, 1))
+                        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+                        merged = tf.summary.merge_all()
+
+                        placeholders = (real_C_pl, imag_C_pl, others_pl, labels_pl)
+
+                        sess.run(tf.global_variables_initializer())
+
+                        test_path = 'tmp/test/logs'
+                        if tf.gfile.Exists(test_path):
+                            tf.gfile.DeleteRecursively(test_path)
+                        tf.gfile.MakeDirs(test_path)
+                        test_writer = tf.summary.FileWriter(test_path, sess.graph)
+
+                        # train
+                        # Train
+                        best_accuracy = 0.0
+                        for step in range(max_step):
+                            before_time = time.time()
+                            data = get_batch_data(train_dataset, batch_size)
+                            # data = get_whole_data(train_dataset)
+                            # index, data = sequence_get_data(train_dataset, index, batch)
 
 
-        W_fc1 = weight_variable([5 * 7 * 64, 1024])
-        b_fc1 = bias_variable([1024])
+                            _, loss_v = sess.run([train_step, cross_entropy],
+                                                 feed_dict={real_C_pl: data['real_C'], imag_C_pl: data['imag_C'],
+                                                            others_pl: data['others'], labels_pl: data['labels'],
+                                                            keep_prob: keep_prob_v})
 
-        h_pool3_flat = tf.reshape(h_pool3, [-1, 5*7*64])
-        h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
+                            if step % 100 == 0:
+                                print 'loss in step %d is %f' % (step, loss_v)
+                                last_time = time.time()
+                                span_time = last_time - before_time
+                                print ('last 100 loop use %f sec' % (span_time * 100))
+                                print ('rest time is %f minutes' % (span_time * (max_step - step) * loop_num / 60))
 
-        keep_prob = tf.placeholder(tf.float32)
-        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+                            if step % 500 == 0 or step == max_step - 1:
+                                result = do_eval(sess, train_dataset, batch_size, accuracy, placeholders,
+                                                 merged, test_writer, False, step)
+                                print '----------train acc in step %d is %f-------------' % (step, result)
+                                result = do_eval(sess, validation_dataset, batch_size, accuracy, placeholders,
+                                                 merged, test_writer, True, step)
+                                print '----------accuracy in step %d is %f-------------' % (step, result)
+                                if result > best_accuracy:
+                                    best_accuracy = result
 
-        W_fc2 = weight_variable([1024, 3])
-        b_fc2 = bias_variable([3])
+                        result = do_eval(sess, test_dataset, batch_size, accuracy, placeholders,
+                                         merged, test_writer, False, step)
+                        print '-----------last accuracy is %f------------' % (result)
 
-        y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+                        filename = '%.2f-%s' % (best_accuracy,situation_now)
+                        f = file(filename, 'w+')
+                        f.write(str(best_accuracy))
+                        f.write(situation_now)
+                        f.close()
 
-        cross_entropy = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(labels=labels_one_hot, logits=y_conv), name='xentropy')
+                        test_writer.close()
 
-        train_step = tf.train.AdamOptimizer(lr_rate).minimize(cross_entropy)
-        correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(labels_one_hot, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-        merged = tf.summary.merge_all()
-
-        placeholders = (real_C_pl, imag_C_pl, others_pl, labels_pl)
-
-        sess.run(tf.global_variables_initializer())
-
-        test_path = 'tmp/test/logs'
-        if tf.gfile.Exists(test_path):
-            tf.gfile.DeleteRecursively(test_path)
-        tf.gfile.MakeDirs(test_path)
-        test_writer = tf.summary.FileWriter(test_path, sess.graph)
-
-        #train
-        # Train
-        for step in range(max_step):
-            before_time = time.time()
-            data = get_batch_data(train_dataset, batch_size)
-            # data = get_whole_data(train_dataset)
-            # index, data = sequence_get_data(train_dataset, index, batch)
-
-
-            _, loss_v = sess.run([train_step, cross_entropy], feed_dict={real_C_pl:data['real_C'], imag_C_pl:data['imag_C'],
-                          others_pl:data['others'], labels_pl:data['labels'], keep_prob:1.0})
-
-            if step % 100 == 0:
-                print 'loss in step %d is %f' % (step, loss_v)
-                last_time = time.time()
-                span_time = last_time - before_time
-                print ('last 100 loop use %f sec' % (span_time * 100))
-                print ('rest time is %f minutes' % (span_time * (max_step - step) / 60))
-
-            if step % 500 == 0 or step == max_step - 1:
-                result = do_eval(sess, train_dataset, batch_size, accuracy, placeholders,
-                                 merged, test_writer, False, step)
-                print '----------train acc in step %d is %f-------------' % (step, result)
-                result = do_eval(sess, validation_dataset, batch_size, accuracy, placeholders,
-                                 merged, test_writer, True, step)
-                print '----------accuracy in step %d is %f-------------' % (step, result)
-
-        result = do_eval(sess, test_dataset, batch_size, accuracy, placeholders,
-                         merged, test_writer, False, step)
-        print '-----------last accuracy is %f------------' % (result)
-
-        test_writer.close()
+                        loop_num -= 1
 
 
 
