f8d1cf65be03462caae4c2f0b7dd5a10382e4c87
freshield <zxdsw199182@gmail.com>
2017-08-01 14:56:02 -0400

    done transfer log 1

--- Project/15_transfer_log/transfer_log1.py
+++ Project/15_transfer_log/transfer_log1.py
@@ -1,28 +1,23 @@
 import numpy as np
 
 filename = '0.9151_epoch99'
-filename1 = 'Link_CNN_10k0.4020_epoch99'
 test_str = '----------epoch 9 test accuracy is 0.851900----------'
 
-accuracy = []
-accuracy1 = []
-context = ''
+context_train = ''
+context_test = ''
 for line in open(filename):
+    if 'train acc in loop 599 is' in line:
+        index = line.find('0.')
+        acc = round(float(line[index:index+5]), 2)
+        context_train += str(acc) + ','
     if 'test accuracy is' in line:
         index = line.find('0.')
         acc = round(float(line[index:index+5]), 2)
-        accuracy.append(acc)
-        context += str(acc) + ','
+        context_test += str(acc) + ','
 
-context += '\n'
+context = ''
+context += context_train + '\n' + context_test
 
-for line in open(filename1):
-    if 'test accuracy is' in line:
-        index = line.find('0.')
-        acc = round(float(line[index:index+5]), 2)
-        accuracy.append(acc)
-        context += str(acc) + ','
 
-print accuracy
-with open('accuracy1.csv', 'wb') as f:
+with open('accuracy2.csv', 'wb') as f:
     f.write(context)
