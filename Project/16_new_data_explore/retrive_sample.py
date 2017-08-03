821036c079a37f51c226b590f449f58fc36e054e
freshield <zxdsw199182@gmail.com>
2017-08-03 15:55:20 -0400

    begin today

--- Project/16_new_data_explore/retrive_sample.py
+++ Project/16_new_data_explore/retrive_sample.py
@@ -3,7 +3,9 @@ import numpy as np
 
 #filename = '/media/freshield/DATA_U/Ciena_data/new_data/FiberID_6fibers_5Spans/FiberID_6fibers_5Spans_noPCA_1.csv'
 #filename = '/media/freshield/DATA_U/Ciena_data/new_data/FiberID_6fibers_10Spans/FiberID_6fibers_10Spans_noPCA_1.csv'
-filename = '/media/freshield/DATA_U/Ciena_data/new_data/FiberID_6fibers_20Spans/FiberID_6fibers_20Spans_noPCA_1.csv'
+#filename = '/media/freshield/DATA_U/Ciena_data/new_data/FiberID_6fibers_20Spans/FiberID_6fibers_20Spans_noPCA_1.csv'
+
+filename = '20spans_sample.csv'
 
 chunkSize = 10
 savename = '20spans_sample.csv'
@@ -12,6 +14,6 @@ reader = pd.read_csv(filename, iterator=True)
 
 chunk = reader.get_chunk(chunkSize)
 
-np.savetxt(savename, chunk, delimiter=",")
+#np.savetxt(savename, chunk, delimiter=",")
 
 print chunk.shape
