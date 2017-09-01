fe87e559b2c09d1659d4ae5ea7944dfef618983e
freshield <zxdsw199182@gmail.com>
2017-09-01 17:21:00 -0400

    done open file test

--- 17_TFRecord/test1.py
+++ 17_TFRecord/test1.py
@@ -2,4 +2,11 @@ import numpy as np
 
 filename = '/media/freshield/BACKUP/FiberID_6fibers_20Spans_noPCA_1.csv'
 
-print filename[:3]
+count = 0
+for line in open(filename, 'r'):
+    print line[:10]
+    count += 1
+    if count == 50:
+        break
+
+print 'done'
