7c015b539b90ad3da3f6829bec2e27c4ed95b71d
freshield <zxdsw199182@gmail.com>
2017-09-03 20:46:17 -0400

    done function

--- 17_TFRecord/csv_to_tfrecord.py
+++ 17_TFRecord/csv_to_tfrecord.py
@@ -1,8 +1,14 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import time
+import os
 
-def csv_to_tfrecord(filename, writer):
+def get_files_name(file_dir):
+    for root, dirs, files in os.walk(file_dir):
+        return files
+
+def csv_to_tfrecord(filename, savename):
 
     def _int64_feature(value):
         return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
@@ -11,6 +17,7 @@ def csv_to_tfrecord(filename, writer):
         return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
     # create writer
+    writer = tf.python_io.TFRecordWriter(savename)
 
     reader = pd.read_csv(filename, header=None, chunksize=1)
 
@@ -44,10 +51,37 @@ def csv_to_tfrecord(filename, writer):
     writer.close()
     print 'Successfully convert'
 
-dir_name = ''
-filename = '/media/freshield/CORSAIR/LEARN_TENSORFLOW/Project/19_ciena_20spans_data/sample/sample_set.csv'
-writer = tf.python_io.TFRecordWriter('data/sample.tfrecords')
+def convert_whole_dir_csv(dir_path, save_path):
+    filename_list = get_files_name(dir_path)
+    total_filenum = len(filename_list)
+    count = 0
+    for filename in filename_list:
+        before_time = time.time()
 
-csv_to_tfrecord(filename, writer)
+        print count
 
-writer.close()
+        #look like Raw_data_xxx_train.csv
+        the_name = filename.split('.')[0]
+        savename = save_path + the_name + '.tfrecords'
+        csv_to_tfrecord(dir_path+filename,savename)
+
+        if count % 50 == 0 and count != 0:
+            span_time = time.time() - before_time
+            print "use %.2f second in 10 loop" % (span_time * 10)
+            print "need %.2f minutes for all loop" % (((total_filenum - count) * span_time) / 60)
+
+        # i += chunk.shape[0]
+        count += 1
+    print 'Done convert'
+
+
+
+
+#dir_path = '/media/freshield/New_2T_Data/Ciena_new_data/20spans/norm/'
+dir_path = 'data/test_fuc/'
+save_path = 'data/test_fuc/'
+
+convert_whole_dir_csv(dir_path,save_path)
+
+
+""""""
