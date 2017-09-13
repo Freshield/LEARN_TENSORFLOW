40a7b1f34486041b9ff80f1110a59540357b1bdf
freshield <zxdsw199182@gmail.com>
2017-09-13 16:39:58 -0400

    update

--- Project/20_otto_test_dataset/otto_cal_recut_norm.py
+++ Project/20_otto_test_dataset/otto_cal_recut_norm.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 
-train_filename = 'data/norm/train_set.csv'
-f_test_filename = 'data/test.csv'
+filename = 'data/train.csv'
 
 dic = {'Class_1':0., 'Class_2':1., 'Class_3':2., 'Class_4':3., 'Class_5':4., 'Class_6':5., 'Class_7':6., 'Class_8':7., 'Class_9':8.}
 
@@ -10,15 +9,20 @@ dic = {'Class_1':0., 'Class_2':1., 'Class_3':2., 'Class_4':3., 'Class_5':4., 'Cl
 def calcul_norm_new(dataset, min, max, mean):
     return (2 * dataset - 2 * mean) / (max - min)
 
-#read train and final test file
-train_data = pd.read_csv(train_filename,header=None)
-test_data = pd.read_csv(f_test_filename)
+data = pd.read_csv(filename)
 
-test_size = 144368
+print data.shape
 
-#the last value is category
-train_set_value = train_data.values[:,:-1].astype(np.float32)
-test_set_value = test_data.values[:,1:].astype(np.float32)
+test_size = 878
+
+indexs = np.arange(len(data))
+np.random.shuffle(indexs)
+
+train_set = data.values[indexs[0:-878],1:]
+test_set = data.values[indexs[-878:],1:]
+
+train_set_value = data.values[indexs[0:-878],1:-1].astype(np.float32)
+test_set_value = data.values[indexs[-878:],1:-1].astype(np.float32)
 
 print train_set_value.shape
 print test_set_value.shape
