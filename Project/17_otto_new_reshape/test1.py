43aeb9511f90f450a237172012045ac0ae40acc5
freshield <zxdsw199182@gmail.com>
2017-09-29 16:50:45 -0400

    change machine

--- Project/17_otto_new_reshape/test1.py
+++ Project/17_otto_new_reshape/test1.py
@@ -4,10 +4,12 @@ a = np.array([1,2,3,4,5,6]).reshape((2,-1))
 
 print a.shape
 
-def reshape_dataset(dataset):
+def triple_size_data(dataset):
+    #a_x is 2
+    #a_y is 3
     a_x, a_y = dataset.shape
 
-    input_data = np.zeros((a_x, a_y, a_y, a_y))
+    output_dataset = np.zeros((a_x, a_y, a_y, a_y))
 
     for batch_num in range(a_x):
 
@@ -16,26 +18,36 @@ def reshape_dataset(dataset):
         raw_data = np.zeros((a_y, a_y))
 
         for j in xrange(a_y):
-            right = a[batch_num, :j]
-            left = a[batch_num, j:]
+            right = dataset[batch_num, :j]
+            left = dataset[batch_num, j:]
             raw_data[j, :] = np.concatenate((left, right))
 
-        print raw_data
+        #print raw_data
 
         # to create 3x3x3 matrix
 
         result = np.zeros((a_y, a_y, a_y))
 
         first = raw_data[0]
-        print first
+        #first is 123;231;312;
+        #print first
 
         for x in range(a_y):
             temp_data = np.zeros((a_y, a_y))
             temp_data[:, :] = raw_data[:, :]
+            #exchange xth and first value
             temp_data[x] = first
             temp_data[0] = raw_data[x]
             result[x, :, :] = temp_data[:, :]
 
-        input_data[batch_num, :, :, :] = result[:, :, :]
+        output_dataset[batch_num, :, :, :] = result[:, :, :]
 
-    print output
+    return output_dataset
+
+output = triple_size_data(a)
+print output
+
+b = np.arange(15).reshape((3,5))
+
+output = triple_size_data(b)
+print output.shape
