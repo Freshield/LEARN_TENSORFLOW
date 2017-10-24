32883efec36ac426c60c88be8eb815b290e991b9
freshield <zxdsw199182@gmail.com>
2017-10-24 19:16:02 -0400

    done the train

--- Project/26_MRQU_project/QU_train.py
+++ Project/26_MRQU_project/QU_train.py
@@ -7,14 +7,14 @@ import numpy as np
 #######################################################
 #from network_model_example import *
 epochs = 200
-data_size = 61000
-file_size = 61000
-test_size = 878
+data_size = 24000
+file_size = 24000
+test_size = 6000
 
-batch_size = 100
+batch_size = 50
 #hypers
-reg = 0.12789
-lr_rate = 0.000094
+reg = 0.000043
+lr_rate = 0.000034
 lr_decay = 0.99
 keep_prob_v = 1.0
 log_dir = 'logs/'
@@ -24,14 +24,25 @@ loop = 0
 best_model_number = 10
 best_model_acc_dic = None
 best_model_dir_dic = None
-train_filename = '/media/freshield/CORSAIR/LEARN_TENSORFLOW/Project/14_otto_single_file/data/norm/train_set.csv'
-test_filename = '/media/freshield/CORSAIR/LEARN_TENSORFLOW/Project/14_otto_single_file/data/norm/test_set.csv'
+train_filename = 'data/clean_raw_train_35.csv'
+test_filename = 'data/clean_raw_test_35.csv'
+ones_filename = 'data/train_label_one_set.csv'
+ones_batch = 40
 ########################################################
 
 
 #######################################################################
 ################      HELPER    #######################################
 #######################################################################
+
+def get_data_from_label_one_set(filename,batch_num):
+    data = pd.read_csv(filename,header=None).values
+
+    indexs = np.arange(data.shape[0])
+    np.random.shuffle(indexs)
+
+    ones_data = data[indexs[:batch_num]]
+    return ones_data
 
 def sequence_get_data(dataset, indexs, last_index, batch_size):
     next_index = last_index + batch_size
@@ -66,7 +77,7 @@ def print_and_log(word, log):
 ################################################################################
 
 # change the dir and log dir nameodule_dir = module_dir + model_name + '/'
-log_dir = log_dir + 'otto_resnet' + '/'
+log_dir = log_dir + 'QU_resnet' + '/'
 
 create_dir(log_dir)
 create_dir(module_dir)
@@ -84,11 +95,16 @@ best_model_dir_dic = []
 for i in range(best_model_number):
     best_model_dir_dic.append('%s'%best_model_acc_dic[i])
 
+# read file
+train_data = pd.read_csv(train_filename).values
+test_data = pd.read_csv(test_filename).values
+test_x, test_y = model.reshape_dataset(test_data)
+
 with tf.Graph().as_default():
     with tf.Session() as sess:
         # inputs
-        input_x = tf.placeholder(tf.float32, [None, 96, 96, 93], name='input_x')
-        input_y = tf.placeholder(tf.float32, [None, 9], name='input_y')
+        input_x = tf.placeholder(tf.float32, [None, 40, 40, 35], name='input_x')
+        input_y = tf.placeholder(tf.float32, [None, 2], name='input_y')
         train_phase = tf.placeholder(tf.bool, name='train_phase')
         keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
@@ -106,10 +122,6 @@ with tf.Graph().as_default():
 
         sess.run(tf.global_variables_initializer())
 
-        # read file
-        train_data = pd.read_csv(train_filename, header=None).values
-        test_data = pd.read_csv(test_filename, header=None).values
-        test_x, test_y = model.reshape_dataset(test_data)
         while epoch < epochs:
 
             print
@@ -135,7 +147,14 @@ with tf.Graph().as_default():
                     last_index = 0
                     out_of_dataset = False
 
-                last_index, data, out_of_dataset = sequence_get_data(train_data, indexs, last_index, batch_size)
+                last_index, raw_data, out_of_dataset = sequence_get_data(train_data, indexs, last_index, batch_size)
+
+                # for balance data
+                ones_data = get_data_from_label_one_set(ones_filename, ones_batch)
+                data = np.zeros((batch_size + ones_batch, ones_data.shape[1]))
+                data[:batch_size, :] = raw_data[:, :]
+                data[batch_size:, :] = ones_data[:, :]
+                np.random.shuffle(data)
 
                 data_x, data_y = model.reshape_dataset(data)
 
