7edb9f7a2bbc7bc15c8c4fd3dffe6bac2fa8cdd4
freshield <zxdsw199182@gmail.com>
2017-11-26 22:06:22 -0500

    update

--- Project/24_ciena_20spans_function/roc_20spans.py
+++ Project/24_ciena_20spans_function/roc_20spans.py
@@ -1,4 +1,18 @@
 import os
+import pandas as pd
+import numpy as np
+
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from scipy import interp
+
 def file_name(file_dir):
     for root, dirs, files in os.walk(file_dir):
         return files
@@ -18,8 +32,39 @@ prob_file_dic = {}
 for i in range(20):
     prob_file_dic[i+1] = files[i]
 
-for num, name in lable_file_dic.items():
-    print num, name
+span_num = 20
 
-for num, name in prob_file_dic.items():
-    print num, name
+score_filename = prob_file_dic[span_num]
+label_filename = lable_file_dic[span_num]
+
+y_score = pd.read_csv(prob_file_dir+score_filename, header=None).values
+y_test = pd.read_csv(lable_file_dir+label_filename, header=None, dtype=np.int32).values
+
+n_classes = 6
+
+#compute ROC
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:,i], y_score[:,i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+#plot
+plt.figure()
+#linewidth
+lw = 2
+
+colors = cycle(['aqua', 'darkorange', 'cornflowerblue','blue','green','red','cyan','magenta','yellow'])
+for i, color in zip(range(n_classes), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+             label='ROC curve of class {0} (area = {1:0.2f})'
+             ''.format(i, roc_auc[i]))
+plt.plot([0,1], [0,1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0,1.0])
+plt.ylim([0.0,1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc='lower right')
+plt.show()
