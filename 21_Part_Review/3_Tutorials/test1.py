9e94b17ecad55ced951f2e494a6fe31ceef3648b
freshield <zxdsw199182@gmail.com>
2018-01-15 15:13:18 +0800

    update

---
+++ 21_Part_Review/3_Tutorials/test1.py
@@ -0,0 +1,29 @@
+import queue
+queue1 = queue.Queue(maxsize = 3)
+queue2 = queue.Queue(maxsize=4)
+
+def safe_put(num, target):
+    if target.full():
+        target.get()
+    target.put(num)
+
+safe_put(1, queue1)
+print()
+print(queue1.qsize())
+print()
+safe_put(2, queue1)
+safe_put(3, queue1)
+safe_put(4, queue1)
+
+for i in range(queue1.qsize()):
+    print(queue1.get())
+
+safe_put(5, queue2)
+safe_put(6, queue2)
+safe_put(7, queue2)
+safe_put(8, queue2)
+safe_put(9, queue2)
+
+print()
+for i in range(queue2.qsize()):
+    print(queue2.get())
